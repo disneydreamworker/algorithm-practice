522dd1f14c773fde460ece640c076391a8e477e1
KeK <127266806+disneydreamworker@users.noreply.github.com>
2024-07-26 12:48:07 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,3 +1,7 @@
 select count(id) as count
 from ECOLI_DATA 
 where GENOTYPE & 2 = 0 and (GENOTYPE & 1 > 0 or GENOTYPE & 4 > 0);
+
+# SELECT COUNT(*) AS "COUNT"
+# FROM ECOLI_DATA
+# WHERE GENOTYPE % 8 IN (1,4,5);
