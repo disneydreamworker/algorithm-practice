5ce26685c75b388ce9b5e18b80f1e1add85dc959
KeK <127266806+disneydreamworker@users.noreply.github.com>
2024-07-27 16:04:31 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,14 +1,44 @@
-SELECT
-    e.ID,
-    CASE 
-        WHEN (e2.NUM / (SELECT COUNT(*) FROM ECOLI_DATA)) * 100 BETWEEN 0 AND 25 THEN 'CRITICAL'
-        WHEN (e2.NUM / (SELECT COUNT(*) FROM ECOLI_DATA)) * 100 <= 50 THEN 'HIGH'
-        WHEN (e2.NUM / (SELECT COUNT(*) FROM ECOLI_DATA)) * 100 <= 75 THEN 'MEDIUM'
-        ELSE 'LOW'
-    END AS 'COLONY_NAME'
-FROM ECOLI_DATA e
-JOIN (
-      SELECT ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) AS NUM, ID, SIZE_OF_COLONY
-      FROM ECOLI_DATA) e2
-ON e.ID = e2.ID
-ORDER BY e.ID;
+-- ROW_NUMBER 사용
+# SELECT
+#     e.ID,
+#     CASE 
+#         WHEN (e2.NUM / (SELECT COUNT(*) FROM ECOLI_DATA)) * 100 BETWEEN 0 AND 25 THEN 'CRITICAL'
+#         WHEN (e2.NUM / (SELECT COUNT(*) FROM ECOLI_DATA)) * 100 <= 50 THEN 'HIGH'
+#         WHEN (e2.NUM / (SELECT COUNT(*) FROM ECOLI_DATA)) * 100 <= 75 THEN 'MEDIUM'
+#         ELSE 'LOW'
+#     END AS 'COLONY_NAME'
+# FROM ECOLI_DATA e
+# JOIN (
+#       SELECT ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) AS NUM, ID, SIZE_OF_COLONY
+#       FROM ECOLI_DATA) e2
+# ON e.ID = e2.ID
+# ORDER BY e.ID;
+
+
+-- RANK 사용
+# SELECT ID, 
+#     CASE WHEN RANK <= ((SELECT COUNT(*) FROM ECOLI_DATA) * 0.25) THEN 'CRITICAL'
+#          WHEN RANK <= ((SELECT COUNT(*) FROM ECOLI_DATA) * 0.50) THEN 'HIGH'
+#          WHEN RANK <= ((SELECT COUNT(*) FROM ECOLI_DATA) * 0.75) THEN 'MEDIUM'
+#          ELSE 'LOW'
+#     END AS COLONY_NAME
+# FROM
+# (
+#     SELECT ID, RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS RANK
+#     FROM ECOLI_DATA
+# ) AS e
+# ORDER BY 1;
+
+
+select a.ID,
+    case 
+        when a.percentover <= 0.25 then 'CRITICAL'
+        when a.percentover <= 0.5 then 'HIGH'
+        when a.percentover <= 0.75 then 'MEDIUM'
+    else 'LOW' end COLONY_NAME
+from
+    (select 
+            ID, 
+            PERCENT_RANK() over (order by SIZE_OF_COLONY desc) percentover
+    from ECOLI_DATA) a
+order by 1;
