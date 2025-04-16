6d8a7fd0bae07c3d14d28555d89b7a5a0da2b81e
KeK <127266806+disneydreamworker@users.noreply.github.com>
2025-04-16 21:31:33 +0900

    [Silver V] Title: 단어 정렬, Time: 328 ms, Memory: 24540 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,37 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeSet;
 
+
 public class Main {
-     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int wordsCnt = sc.nextInt();
-    sc.nextLine();
 
-    TreeSet<String> treeSet = new TreeSet<>();
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    while (sc.hasNext()) {
-      treeSet.add(sc.nextLine());
+    int n = Integer.parseInt(br.readLine());
+    Set<String> result = new TreeSet<>();
+
+    while (n-- > 0) {
+      result.add(br.readLine());
     }
 
-    List<String> list = new ArrayList<>(treeSet);
-
-    Collections.sort(list, new Comparator<String>() {
+    ArrayList<String> arrayList = new ArrayList<>(result);
+    Collections.sort(arrayList, new Comparator<String>() {
       @Override
       public int compare(String o1, String o2) {
-        if (o1.length() == o2.length()) {
-          return o1.compareTo(o2);
-        }
+        if (o1.length() == o2.length()) return o1.compareTo(o2);
         return o1.length() - o2.length();
       }
     });
 
-    for (String word : list) {
-      System.out.println(word);
+    StringBuilder sb = new StringBuilder();
+    for (String s : arrayList) {
+      sb.append(s).append("\n");
     }
+
+    System.out.print(sb);
   }
-    
 }
