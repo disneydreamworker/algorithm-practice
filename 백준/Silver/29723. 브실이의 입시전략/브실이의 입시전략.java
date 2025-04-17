af008fa685069cf20618b4e29d1fa577d4ff84b4
KeK <127266806+disneydreamworker@users.noreply.github.com>
2025-04-17 17:43:12 +0900

    [Silver V] Title: 브실이의 입시전략, Time: 248 ms, Memory: 21372 KB -BaekjoonHub

--- 백준/Silver/29723. 브실이의 입시전략/브실이의 입시전략.java
+++ 백준/Silver/29723. 브실이의 입시전략/브실이의 입시전략.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-  public static HashMap<String, Integer> getHashMap(String line, HashMap<String, Integer> map) {
+  public static HashMap<String,Integer> getHashMap(String line, HashMap<String, Integer> map) {
     StringTokenizer st = new StringTokenizer(line);
     map.put(st.nextToken(), Integer.parseInt(st.nextToken()));
     return map;
@@ -17,47 +17,46 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken()); // 브실이가 수강한 과목 수
-    int m = Integer.parseInt(st.nextToken()); // 브실대학에서 요구하는 과목 수
-    int k = Integer.parseInt(st.nextToken()); // 브실대학에서 공개한 과목 수
+    int n = Integer.parseInt(st.nextToken()); //브실이가 수강한 과목 수
+    int m = Integer.parseInt(st.nextToken()); //브실대학에서 요구하는 과목 수
+    int k = Integer.parseInt(st.nextToken()); //브실대학에서 공개한 과목 수
 
     HashMap<String, Integer> map = new HashMap<>();
-    for (int i = 0; i < n; i++) {
+    while (n-- > 0) {
       String line = br.readLine();
       map = getHashMap(line, map);
     }
 
     int answer = 0;
-    ArrayList<String> openSubjects = new ArrayList<>();
-    
+
     for (int i = 0; i < k; i++) {
-      String subject = br.readLine();
-      openSubjects.add(subject);
-      answer += map.get(subject);
+      String line = br.readLine();
+      answer += map.get(line);
+      map.remove(line);
     }
     
-    // 공개된 과목들을 map에서 제거
-    for (String subject : openSubjects) {
-      map.remove(subject);
-    }
-    
-    // 남은 과목들의 점수를 리스트에 저장
-    ArrayList<Integer> remainingScores = new ArrayList<>(map.values());
-    Collections.sort(remainingScores);
-    
+    /*
+    while (k-- > 0) {
+      String line = br.readLine();
+      answer += map.get(line);
+      map.remove(line);
+    }*/
+
+    ArrayList<Integer> list = new ArrayList<>(map.values());
+    Collections.sort(list);
+
     int min = answer;
     int max = answer;
-    
-    // 최소 점수: 가장 작은 m-k개의 점수를 더함
+
     for (int i = 0; i < m - k; i++) {
-      min += remainingScores.get(i);
+      min += list.get(i);
     }
-    
-    // 최대 점수: 가장 큰 m-k개의 점수를 더함
-    for (int i = 0; i < m - k; i++) {
-      max += remainingScores.get(remainingScores.size() - 1 - i);
+
+    for (int i = list.size() - 1; i >= list.size() - (m - k); i--) {
+      max += list.get(i);
     }
 
     System.out.println(min + " " + max);
+
   }
 }
